e6c893d60d4566ed8291d8148b98981d2f35b2bf
Pipin 18 <pipin1288@gmail.com>
2022-11-19 16:28:47 +0700

    kelompok 8

---
+++ OLSHOPORIFLAME/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from.views import *
+
+
+app_name = "OLSHOPORIFLAME"
+urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
+    path("about/", AboutView.as_view(), name="about"),
+    path("contact-us/", ContactView.as_view(), name="contact"),
+    path("all-products/", AllProductsView.as_view(), name="allproducts"),
+    path("product/<slug:slug>/", ProductDetailView.as_view(), name="productdetail"),
+
+
+]
